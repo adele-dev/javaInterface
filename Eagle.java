eb25173e948c210a65cd55db1925a5f5de94eadc
Adèle <aguessya@yahoo.fr>
2022-07-31 17:16:54 +0200

    interface fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,29 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.printf("%s is going to fly!%n", this.getName());
+    }
+
+    @Override
+    public int ascend(int meters){
+        this.altitude  = this.altitude + meters;
+        System.out.printf("%s is ascending and his new altitude is %s meters%n", this.getName(), meters);
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters){
+        this.altitude  = this.altitude - meters;
+        System.out.printf("%s is descending and his new altitude is %s meters%n", this.getName(), meters);
+        return this.altitude;
+    }
+
+    @Override
+    public void land(){
+        System.out.printf("%s is about to land now :)%n", this.getName());
     }
 }
